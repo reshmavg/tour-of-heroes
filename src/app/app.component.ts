a02cf8249fb39d1b5dda8b7c7b87e7abd1127258
Reshma Vemula <rgajam.v@gmail.com>
2016-07-19 23:02:43 -0700

    Updated to ng RC4. Added hero editor, master/detail, multiple component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { Hero } from './shared/hero.model';
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  selectedHero: Hero;
+  public heroes = HEROES;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
 }
---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Hero } from '../shared/hero.model';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: ['hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input()
+  hero: Hero;
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
